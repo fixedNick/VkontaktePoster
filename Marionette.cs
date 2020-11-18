05540f7a0c1833039840117b52fc7fa43c60d4be
kos <kkos44911@gmail.com>
2020-11-18 16:17:18 +0300

    Добавлен класс Marionette, используется для манипуляций с браузером. Так же в нем описан класс DriverSettings, используемый для передачи настроек драйвера.

---
+++ Marionette.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeleniumDriver;
+
+namespace VkontaktePoster
+{
+    class Marionette
+    {
+        private DriverSettings settings;
+        private Driver driver;
+
+        public Marionette(DriverSettings driverSettings)
+        {
+            settings = driverSettings;
+        }
+
+        /// <summary>
+        /// Initialize driver settings and start driver.
+        /// </summary>
+        public void Initialize()
+        {
+            driver = new Driver(settings.SeleniumDriverType, settings.DriverNotificationDelegate, settings.StartMaximized, settings.Headless, settings.HidePrompt, settings.ShowExceiptions, settings.DriverFileName);
+        }
+    }
+
+    /// <summary>
+    /// Settings to start new SeleniumDriver
+    /// </summary>
+    class DriverSettings
+    {
+        public readonly Driver.DriverType SeleniumDriverType;
+        public readonly Driver.NotificationDelegate DriverNotificationDelegate;
+        public readonly Boolean StartMaximized;
+        public readonly Boolean Headless;
+        public readonly Boolean HidePrompt;
+        public readonly Boolean ShowExceiptions;
+        public readonly String DriverFileName;
+
+        public DriverSettings(Driver.DriverType type, Driver.NotificationDelegate notificationHandler, bool startMaximized, bool headless, bool hidePrompt, bool showExceptions, string driverName = "chromedriver.exe")
+        {
+            SeleniumDriverType = type;
+            DriverNotificationDelegate = notificationHandler;
+            StartMaximized = startMaximized;
+            Headless = headless;
+            HidePrompt = hidePrompt;
+            ShowExceiptions = showExceptions;
+            DriverFileName = driverName;
+        }
+    }
+}
